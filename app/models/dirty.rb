1ca1b83d40036345cba5292b97847fcaa99e9ff6
Robbie Paul <robbiepaul@me.com>
2021-06-10 15:18:09 +0100

    Add Rubocop

--- app/models/dirty.rb
+++ app/models/dirty.rb
@@ -1,48 +1,46 @@
 class Dirty
   def awful(x, y)
     if y
-      @screen = widgets.map {|w| w.each {|key| key += 3}}
+      @screen = widgets.map { |w| w.each { |key| key += 3 } }
       return true
       if x
         @screen = nil
 
-        unless y.empty? && x.empty? && @screen && widgets.any?
-          @x = y * 3
-        end   
+        @x = y * 3 unless y.empty? && x.empty? && @screen && widgets.any?
       end
     end
     @x = 10_000_000
-    @foo = "bar"
-    return "1" unless x == y
-    
-    return []
+    @foo = 'bar'
+    return '1' unless x == y
+
+    []
   end
 
   def too_long
     arr = []
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
-    arr << "1"
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
+    arr << '1'
     arr
   end
 end
